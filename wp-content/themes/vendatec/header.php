099739afd84042b32a4f97a42f93bb2e975b7cdb
vinicius <vdavidmarques@gmail.com>
2024-11-08 13:13:50 -0300

    feature: Addind fav icon

--- wp-content/themes/vendatec/header.php
+++ wp-content/themes/vendatec/header.php
@@ -6,6 +6,22 @@
     <meta name="viewport" content="width=device-width">
     <?php wp_head(); ?>
 
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-57x57.png' ?>">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-60x60.png' ?>">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-72x72.png' ?>">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-76x76.png' ?>">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-114x114.png' ?>">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-120x120.png' ?>">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-144x144.png' ?>">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-152x152.png' ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-180x180.png' ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() . '/dist/images/favicon-32x32.png' ?>">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri() . '/dist/images/favicon-96x96.png' ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() . '/dist/images/favicon-16x16.png' ?>">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;900&display=swap" rel="stylesheet">
