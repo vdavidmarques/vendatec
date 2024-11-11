6824310012a2339c24d5b4e5a01baa79014188d8
vinicius <vdavidmarques@gmail.com>
2024-11-11 17:40:26 -0300

    chore: Updating components

--- wp-content/themes/vendatec/components/about/content-about.php
+++ wp-content/themes/vendatec/components/about/content-about.php
@@ -1,14 +1,53 @@
 <section class="content-about">
-    <?php 
-        echo get_field('title-wwa');
-        echo get_field('desc-wwa');
-        echo get_field('tag-wwa-values');
-        echo get_field('title-wwa-values');
-        echo get_field('desc-wwa-values');
-        $listWWAValues = get_field('list-wwa-values');
-
-        foreach($listWWAValues as $listWWAValue):
-            echo $listWWAValue['content-wwa-values'];
-        endforeach;
-    ?>
+    <div class="content-about--header">
+        <div class="container">
+            <article class="content-about--header--texts">
+                <div class="title-wwa">
+                    <h2><?php echo get_field('title-wwa'); ?></h2>
+                </div>
+                <div class="desc-wwa">
+                    <?php echo get_field('desc-wwa'); ?>
+                </div>
+            </article>
+        </div>
+    </div>
+    <article class="content-about--values">
+        <div class="content-about--header">
+            <div class="container">
+                <div class="content-about--header--texts">
+                    <div class="title-wwa-values-container">
+                        <div class="tag">
+                            <h3><?php echo get_field('tag-wwa-values'); ?></h3>
+                        </div>
+                        <div class="title-wwa">
+                            <h2><?php echo get_field('title-wwa-values'); ?></h2>
+                        </div>
+                    </div>
+                    <div class="desc-wwa">
+                        <?php echo get_field('desc-wwa-values'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+    <div class="content-about--list">
+        <div class="container content-about--list--container">
+            <?php
+            $listWWAValues = get_field('list-wwa-values');
+            $counter = 1;
+            if ($listWWAValues):
+                foreach ($listWWAValues as $listWWAValue):
+                    ?>
+                <article class="content-about--list--container--items item-<?php echo $counter ?>">
+                    <div class="content-about--list--container--items--texts">
+                        <?php echo $listWWAValue['content-wwa-values']; ?>
+                    </div>
+                </article>
+            <?php
+                $counter++;
+                endforeach;
+            endif;
+            ?>
+        </div>
+    </div>
 </section>
--- wp-content/themes/vendatec/components/secundary-banner.php
+++ wp-content/themes/vendatec/components/secundary-banner.php
@@ -1,16 +1,25 @@
 <section class="secundary-banner">
     <?php
     $mainBanners = get_field('sec-banner');
+    if($mainBanners):
     foreach ($mainBanners as $mainBanner):
     ?>
         <div class="main-banner-item">
-            <img src="<?php echo $mainBanner['image']['url']; ?>" alt="Soluções personalizadas para Bancos de Sangue, Agências Transfusionais e Laboratórios de Hematologia e Hemoterapia">
-            <div class="main-banner-text">
-                <h2><?php echo $mainBanner['main-text']; ?></h2>
-                <?php if ($mainBanner['link-option']): ?>
-                    <a href="<?php echo $mainBanner['button']['url']; ?>" class="btn"><?php echo $mainBanner['button']['title']; ?></a>
-                <?php endif; ?>
+            <div class="main-banner-item--bg">
+                <div class="bg-linear">&nbsp;</div>
+                <img src="<?php echo $mainBanner['image']['url']; ?>" alt="Soluções personalizadas para Bancos de Sangue, Agências Transfusionais e Laboratórios de Hematologia e Hemoterapia" class="image">
+            </div>
+            <div class="container">
+                <div class="main-banner-item--text">
+                    <div class="main-banner-item--title"><?php echo $mainBanner['main-text']; ?></div>
+                    <?php if ($mainBanner['link-option']): ?>
+                        <a href="<?php echo $mainBanner['button']['url']; ?>" class="main-banner-item--button button button--primary button--arrow button--arrow-down"><?php echo $mainBanner['button']['title']; ?></a>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
-    <?php endforeach; ?>
+    <?php 
+        endforeach; 
+     endif; 
+    ?>
 </section>
