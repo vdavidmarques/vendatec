3204e5b7070bc649fc82bee0633526493f47ca83
vdavidmarques <vdavidmarques@gmail.com>
2024-12-19 12:20:25 -0300

    feat: Creating favicon as independent component

---
+++ wp-content/themes/vendatec/services/favicon.php
@@ -0,0 +1,12 @@
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-57x57.png' ?>">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-60x60.png' ?>">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-72x72.png' ?>">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-76x76.png' ?>">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-114x114.png' ?>">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-120x120.png' ?>">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-144x144.png' ?>">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-152x152.png' ?>">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/dist/images/apple-icon-180x180.png' ?>">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() . '/dist/images/favicon-32x32.png' ?>">
+<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri() . '/dist/images/favicon-96x96.png' ?>">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() . '/dist/images/favicon-16x16.png' ?>">
